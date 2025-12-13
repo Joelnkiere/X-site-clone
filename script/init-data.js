c335dc6d7aeb7d6a45428fee59db0b8c64a0a6bd
Joel Nkiere <nkierejoe@gmail.com>
2025-12-13 23:10:02 +0100

    #

--- script/init-data.js
+++ script/init-data.js
@@ -1,10 +1,9 @@
 const fs = require("fs");
 const path = require("path");
 
-const src = path.join(__dirname, "../database/db.example.json");
-const dest = path.join(__dirname, "../database/db.json");
+const src = path.join(__dirname, "../database/db.json");
 
-fs.copyFile(src, dest, (err) => {
+fs.copyFile(src, src, (err) => {
   if (err) {
     console.error("Erreur lors de la copie:", err);
   } else {
